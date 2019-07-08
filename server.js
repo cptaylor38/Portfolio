6d7ab772c9524573f1ecabc6498cf90c9a1cd580
Corbin Taylor <cptaylor38@yahoo.com>
2019-07-08 00:27:30 -0400

    deploy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
-    app.use('/', express.static(path.join(__dirname, "client/build")));
+    app.use('/', express.static(path.join(__dirname, "client/build", 'index.html')));
 }
 
 app.post('/api/form', (req, res) => {
