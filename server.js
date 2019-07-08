5bc535cd414500e62eb4b244a095507f88334d9f
Corbin Taylor <cptaylor38@yahoo.com>
2019-07-07 23:38:12 -0400

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express();
 
