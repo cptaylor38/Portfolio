5ff25f7e42e4a65e715cdc248b7b999b9047cad1
Corbin Taylor <cptaylor38@yahoo.com>
2019-07-08 00:20:49 -0400

    deploy

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err, account) => {
