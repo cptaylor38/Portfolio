8a39b45e6f4b18a4ff494cf63ca0ffd4af3e0a6e
Corbin Taylor <cptaylor38@yahoo.com>
2019-07-07 23:35:59 -0400

    deploying

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err, account) => {
         const htmlEmail = `
